630af3f363d313da0aba8f12325353ae90613476
Wolf Pinz <wolfpinz@gmail.com>
2022-05-24 12:21:14 +0200

    validations finished

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
 
-
-  validates :status, inclusion: { in: w%[pending, accepted, denied] }
+  validates :start_date, :end_date, presence: true
+  validates :status, inclusion: { in: %w[pending accepted denied] }
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,5 +2,6 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :city, :street, :price_per_night, presence: true
+  validates :city, :street, :price_per_night, :description, presence: true
+  validates :description, length: { minimum: 30 }
 end
