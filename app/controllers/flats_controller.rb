d38615086026c48a370ec3b88326bb89178d7f6c
Wolf Pinz <wolfpinz@gmail.com>
2022-05-25 11:50:37 +0200

    added basic flat create

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,12 +2,37 @@ class FlatsController < ApplicationController
   # create views for index, show and create
   # Daniel show
   # Lara index
-  skip_before_action :authenticate_user!
+  # Wolf new, create
+  skip_before_action :authenticate_user!, except: [:create]
+  before_action :set_flat, only: [:show]
   def index
     @flats = Flat.all
   end
 
   def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create!(flat_params)
+
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :city, :street, :price_per_night)
   end
 end
